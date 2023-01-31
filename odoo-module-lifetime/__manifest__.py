dcd11db8982e5cc6eeb7e7ff48d3694470ec638b
NamciMaher <106387374+NamciMaher@users.noreply.github.com>
2023-01-31 13:32:43 +0100

    Add files via upload

---
+++ odoo-module-lifetime/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author      : LamLTD
+# Copyright(c): 2023-Present LamLTD Software Pvt. Ltd.
+# All Rights Reserved.
+#
+#
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#
+# You should have received a copy of the License along with this program.
+#################################################################################
+
+{
+    'name': "Product Lifetime",
+    'summary': "Calculate the lifetime of a product",
+    'version': '15.0.1.0.0',
+    'author': "LamLTD",
+    'category': 'Inventory',
+    'depends': ['stock'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
+
